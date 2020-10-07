1bfd959f2415f3507928fe7983b051cbc8852fc0
SuenjeAlice <g00363332@gmit.ie>
2020-10-07 13:56:29 +0100

    Lab 2 complete

--- bmi.js
+++ bmi.js
@@ -1,15 +1,25 @@
+//Data Representation and Querying - Lab 2 - Exercise 2 - Sünje Ursa Alice Winteler - G00363332 
+
+//Create BMI class
 class BMI{
+
+    //create constructor and add parameters height and weight
     constructor(height, weight){
+        //create local instances of variables
         this.height = height;
         this.weight = weight;
     }
 
+    //create calculateBMI method
     calculateBMI(){
-        let bmi = this.weight / (this.height ** 2);
-        return bmi;
+        //create bmi variable 
+        let bmi = this.weight / (this.height ** 2); //add in the formular to calculate bmi
+        return bmi; //return the result calculated in this method
     }
 }
 
+//create instance of BMI class and add in values for parameters
 let MyBmi = new BMI(2, 90);
+//use MyBMI object and call calculateBMI method
 let calculatedBMI = MyBmi.calculateBMI();
-console.log(calculatedBMI);
+console.log(calculatedBMI); //use console.log to print the result to the console
--- cars.js
+++ cars.js
@@ -1,30 +1,46 @@
-class Verhicle{
+//Data Representation and Querying - Lab 2 - Exercise 3 - Sünje Ursa Alice Winteler - G00363332 
+
+//create Vehicle class
+class Vehicle{
+
+    //create constructor and add parameters make, model and year
     constructor(make, model, year){
+        //create local instances of variables
         this.make = make;
         this.model = model;
         this.year = year;
     }
 
+    //create Information method
     Information(){
-        //Using a German keyboard, can't use slanted quotation marks with it
-        console.log('Make: ' + this.make + ', Model: ' + this.model + ', Year: ' + this.year);
+        /*Using a German keyboard, can't use slanted quotation marks with it, 
+        so using single quotation marks with plus sign instead of using 
+        slanted quotation marks and ${this.variable}*/
+        console.log('Make: ' + this.make + ', Model: ' + this.model + ', Year: ' + this.year);//use console.log to print result to console
     }
 }
 
-//let myVehicle = new Verhicle('VW', 'Golf', 2008);
+//test Vehicle class by creating an instance of Vehicle class and using that to call Information method
+//let myVehicle = new Vehicle('VW', 'Golf', 2008);
 //let info = myVehicle.Information();
 
-class Cars extends Verhicle{
+//create Cars class which inherits from the Vehicle class using extends keyword
+class Cars extends Vehicle{
+
+    //create constructor and add parameters make, model, year and doors
     constructor(make, model, year, doors){
-        super(make, model, year);
+         //create local instances of variables
+        super(make, model, year);//use super keyword to use variables from parent class
         this.doors = doors;
     }
 
+    //create Information method
     Information(){
-        super.Information();
-        console.log('Number of doors: ' + this.doors);
+        super.Information();//use super keyword to use method from parent class
+        console.log('Number of doors: ' + this.doors);//use console.log to print result to console
     }
 }
 
+//create instance of Cars class and add values for parameters
 let myCar = new Cars( 'VW', 'Golf', 2011, 5)
-let info = myCar.Information();
+let info = myCar.Information();//call Information method which prints results to console
